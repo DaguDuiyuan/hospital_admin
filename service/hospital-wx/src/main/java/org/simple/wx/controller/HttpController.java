0000ecfc20ffff96120b9063ded4f88f4de1d841
dagu <1184313565@qq.com>
2023-03-10 21:47:49 +0800

    update

--- service/hospital-wx/src/main/java/org/simple/wx/controller/HttpController.java
+++ service/hospital-wx/src/main/java/org/simple/wx/controller/HttpController.java
@@ -1,6 +1,7 @@
 package org.simple.wx.controller;
 
 import cn.dev33.satoken.annotation.SaIgnore;
+import com.alibaba.fastjson2.JSONObject;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.simple.wx.entity.HttpEntity;
 import org.simple.wx.enums.HttpMethods;
@@ -8,6 +9,9 @@ import org.simple.wx.service.HttpService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -21,11 +25,13 @@ public class HttpController {
     @GetMapping("/{frpName}/{path}")
     public Map<String, Object> httpGet(
             @PathVariable String frpName,
-            @PathVariable String path
+            @PathVariable String path,
+            @RequestParam Map<String, Object> data
     ) {
         return httpService.getHttpResult(HttpEntity.builder()
                 .url("/" + path)
                 .frpName(frpName)
+                .data(data)
                 .method(HttpMethods.GET)
                 .build()).toMap();
     }
@@ -34,14 +40,53 @@ public class HttpController {
     public Map<String, Object> httpPost(
             @PathVariable String frpName,
             @PathVariable String path,
-            @RequestBody Map<String, Object> data
+            HttpServletRequest request
     ) {
         return httpService.getHttpResult(HttpEntity.builder()
                 .url("/" + path)
                 .frpName(frpName)
                 .method(HttpMethods.POST)
-                .data(data)
+                .data(parseMap(request))
                 .build()).toMap();
+    }
+
+    public Map<String, Object> parseMap(HttpServletRequest request){
+        try {
+            return JSONObject.parseObject(getRequestPostStr(request));
+        }catch (Exception e){
+            return new HashMap<>();
+        }
+    }
+
+    public static String getRequestPostStr(HttpServletRequest request)
+            throws IOException {
+        byte[] buffer = getRequestPostBytes(request);
+        String charEncoding = request.getCharacterEncoding();
+        if (charEncoding == null) {
+            charEncoding = "UTF-8";
+        }
+        if (buffer == null) return null;
+        return new String(buffer, charEncoding);
+    }
+
+
+    public static byte[] getRequestPostBytes(HttpServletRequest request)
+            throws IOException {
+        int contentLength = request.getContentLength();
+        if (contentLength < 0) {
+            return null;
+        }
+        byte[] buffer = new byte[contentLength];
+        for (int i = 0; i < contentLength; ) {
+
+            int readlen = request.getInputStream().read(buffer, i,
+                    contentLength - i);
+            if (readlen == -1) {
+                break;
+            }
+            i += readlen;
+        }
+        return buffer;
     }
 
 }
